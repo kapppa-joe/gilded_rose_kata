ac0755c4b850214352a192edaf11b75d1029f539
Joe Fong <joe.fong@madetech.com>
2022-03-01 20:04:59 +0000

    Change texttest_fixture to require from main code correctly

--- spec/texttest_fixture.rb
+++ spec/texttest_fixture.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/ruby -w
 
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+# require File.join(File.dirname(__FILE__), 'gilded_rose')
+require '../lib/gilded_rose'
+
 
 puts "OMGHAI!"
 items = [
